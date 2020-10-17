4f75573d8c33bb4e7a07b9c40db27513014498ca
DuatTT <trantienduat98@gmail.com>
2020-10-17 16:06:51 +0700

    update dto for swagger view

--- src/main/java/org/avengers/capstone/hostelrenting/dto/contract/ContractDTOCreate.java
+++ src/main/java/org/avengers/capstone/hostelrenting/dto/contract/ContractDTOCreate.java
@@ -1,19 +1,14 @@
 package org.avengers.capstone.hostelrenting.dto.contract;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
-import org.avengers.capstone.hostelrenting.dto.group.GroupServiceDTOCreate;
 import org.avengers.capstone.hostelrenting.model.Contract;
-import org.avengers.capstone.hostelrenting.model.GroupService;
 
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
-import java.util.Random;
 import java.util.UUID;
 
-@Getter
-@Setter
 public class ContractDTOCreate {
 
     public ContractDTOCreate() {
@@ -21,36 +16,50 @@ public class ContractDTOCreate {
         this.status = Contract.STATUS.INACTIVE;
         this.createdAt = System.currentTimeMillis();
     }
-
+    @Getter @Setter
     private Integer contractId;
 
+    @Getter @Setter
     @NotNull(message = "Room id is mandatory!")
     private Integer roomId;
 
+    @Getter @Setter
     @NotNull(message = "Vendor id is mandatory!")
     private Long vendorId;
 
+    @Getter @Setter
     @NotNull(message = "Renter id is mandatory!")
     private Long renterId;
 
+    @Getter @Setter
     private Integer dealId;
 
+    @Getter @Setter
     private Integer bookingId;
 
+    @Getter @Setter
     @NotNull(message = "Start time is mandatory!")
     private Long startTime;
 
+    @Getter @Setter
     @NotNull(message = "Contract duration is mandatory!")
     private float duration;
 
+    @Getter @Setter
     private String evidenceImgUrl;
 
+    @Getter
     private Long createdAt;
 
+    @Getter
     private Contract.STATUS status;
 
+    @Getter
     private UUID qrCode;
 
+    @Getter @Setter
     @NotNull(message = "List of agreement services is mandatory!")
+    @JsonProperty(value = "groupServiceIds")
     private Collection<GroupServiceDTOForContract> groupServices;
+
 }
--- src/main/java/org/avengers/capstone/hostelrenting/dto/contract/GroupServiceDTOForContract.java
+++ src/main/java/org/avengers/capstone/hostelrenting/dto/contract/GroupServiceDTOForContract.java
@@ -1,10 +1,14 @@
 package org.avengers.capstone.hostelrenting.dto.contract;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author duattt on 10/17/20
  * @created 17/10/2020 - 15:56
  * @project youthhostelapp
  */
+@Getter @Setter
 public class GroupServiceDTOForContract {
     private Integer groupServiceId;
 }
