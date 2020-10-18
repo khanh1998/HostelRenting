9399ad0521083c209e980e0b6ae1f4fd9a19e1ea
DuatTT <trantienduat98@gmail.com>
2020-10-18 14:38:02 +0700

    config null response for vendor dto

--- src/main/java/org/avengers/capstone/hostelrenting/dto/vendor/ResVendorDTO.java
+++ src/main/java/org/avengers/capstone/hostelrenting/dto/vendor/ResVendorDTO.java
@@ -9,7 +9,6 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResVendorDTO extends UserDTOResponse implements Serializable {
 
 //    private List<ContractDTOShort> contracts;
