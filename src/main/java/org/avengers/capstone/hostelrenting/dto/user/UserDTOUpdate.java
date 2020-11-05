2db8d41d35fa452090407eaa77afd845b47bf0f3
DuatTT <trantienduat98@gmail.com>
2020-11-05 11:14:26 +0700

    Update firebaseToken of user

--- src/main/java/org/avengers/capstone/hostelrenting/dto/user/UserDTOUpdate.java
+++ src/main/java/org/avengers/capstone/hostelrenting/dto/user/UserDTOUpdate.java
@@ -22,6 +22,7 @@ public class UserDTOUpdate implements Serializable {
     @Getter
     @Setter
     private String username;
+
     @Getter
     @Setter
     @NotNull(message = "Year of birth is mandatory!")
@@ -71,6 +72,10 @@ public class UserDTOUpdate implements Serializable {
     @Setter
     @NotNull(message = "Avatar is mandatory!")
     private String avatar;
+
+    @Getter
+    @Setter
+    private String firebaseToken;
 
     @Getter
     @JsonIgnore
