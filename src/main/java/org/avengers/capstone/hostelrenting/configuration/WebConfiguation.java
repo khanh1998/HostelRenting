1aa64f636c047498d9eb9932370d1d021c6d5dc0
DuatTT <trantienduat98@gmail.com>
2020-11-09 09:52:15 +0700

    update cors

--- src/main/java/org/avengers/capstone/hostelrenting/configuration/WebConfiguation.java
+++ src/main/java/org/avengers/capstone/hostelrenting/configuration/WebConfiguation.java
@@ -20,7 +20,7 @@ public class WebConfiguation implements WebMvcConfigurer {
                         "https://hotel-renting-develop.netlify.app",
                         "http://127.0.0.1:8080",
                         "https://hostel-renting.azurewebsites.net",
-                        "https://blue-stone-032f5e600.azurestaticapps.net "
+                        "https://blue-stone-032f5e600.azurestaticapps.net"
                 ).allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("Content-Type", "Origin", "Authorization");
     }
