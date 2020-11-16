78116364109c19dad75a5f14c5592923c122fab1
DuatTT <trantienduat98@gmail.com>
2020-11-16 13:44:27 +0700

    school model

---
+++ src/main/java/org/avengers/capstone/hostelrenting/model/School.java
@@ -0,0 +1,35 @@
+package org.avengers.capstone.hostelrenting.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "school")
+public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int schoolId;
+
+    @Column(nullable = false)
+    private String schoolName;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Renter> renters;
+
+    @ManyToOne
+    @JoinColumn(name = "district_id")
+    private District district;
+}
