0a7a0b2c24d7d86385473977ac7c54c3ca7c01b8
DuatTT <trantienduat98@gmail.com>
2020-12-16 12:06:49 +0700

    find hostel-type by UCategoryIds

--- src/main/java/org/avengers/capstone/hostelrenting/service/UtilityService.java
+++ src/main/java/org/avengers/capstone/hostelrenting/service/UtilityService.java
@@ -1,10 +1,13 @@
 package org.avengers.capstone.hostelrenting.service;
 
 import org.avengers.capstone.hostelrenting.model.UCategory;
+import org.avengers.capstone.hostelrenting.model.Utility;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 public interface UtilityService {
     Set<UCategory> getNearbyUtilities(Double latitude, Double longitude, Double distance);
+    Collection<Utility> getUtilitiesByCategoryId(Integer uCategoryId);
 }
